1a9009a309881a15da7cb87dc416f297613b50bd
Jinpeng Zhang <jimpo.jpz@gmail.com>
2022-11-29 20:14:04 +0100

    Quarta raccolta dati 29/11/2022 8:10PM

--- SocialNetworkAnalysisProject/AccessingTwitterData.R
+++ SocialNetworkAnalysisProject/AccessingTwitterData.R
@@ -256,6 +256,9 @@ write_xlsx(data_frame_1_29112022, "data_frame_1_29112022.xlsx")
 data_frame_2_29112022 <- rtweet::search_tweets("Just stop oil OR just stop oil", n = 20000, retryonratelimit = TRUE) #nolint
 write_xlsx(data_frame_2_29112022, "data_frame_2_29112022.xlsx")
 
+data_frame_3_29112022 <- rtweet::search_tweets("Climate activists OR climate activists", n = 20000, retryonratelimit = TRUE) #nolint
+write_xlsx(data_frame_3_29112022, "data_frame_3_29112022.xlsx")
+
 #test grafo con il dataframe activism
 activism <- read_excel("C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\DataFrames\\.xlsx\\activism_no_duplicates.xlsx")
 activism_data = data.frame(activism)
