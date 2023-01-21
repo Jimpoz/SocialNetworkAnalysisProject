48dde869b52f205918b55cca085e859d47f54bc9
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-21 21:11:34 +0100

    Pulizia file R Rimozione comandi riguardandi salvataggio di dataframe in file xlsx Rimozione comandi test del plot in python

--- SocialNetworkAnalysisProject/WorkingWithDataframes.py
+++ SocialNetworkAnalysisProject/WorkingWithDataframes.py
@@ -1,9 +1,4 @@
 #Questo file conterrà il codice in python utilizzato per creare grafi dai dataframe creati nel file in R
-import json
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-plt.show()
