35083107ca0d220fbb39368c8d575365349bb1f9
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-21 21:12:16 +0100

    Pulizia programma

--- SocialNetworkAnalysisProject/WorkingWithTwitterData.R
+++ SocialNetworkAnalysisProject/WorkingWithTwitterData.R
@@ -97,40 +97,6 @@ axis(1, xaxp=c(0.001,1.000,2), las=1)
 #i dati con cui abbiamo lavorato contengono tutti i tweet che discutono del tema
 #creazione dataframe che contenga tweet riguardanti il movimento Just Stop Oil oppure The Last Generation oppure gli attacchi alle opere d'arte
 
-#per evitare di creare conflitti con troppi OR creeremo più dataframe per poi unirli
-tweets_regarding_movement <- tweetdatand[grep("#JustStopOil", tweetdatand$full_text),]
-tweets_regarding_movement_1 <- tweetdatand[grep("Just Stop Oil", tweetdatand$full_text),]
-tweets_regarding_movement_2 <- tweetdatand[grep("#VanGogh", tweetdatand$full_text),]
-tweets_regarding_movement_3 <- tweetdatand[grep("#ClaudeMonet", tweetdatand$full_text),]
-tweets_regarding_movement_4 <- tweetdatand[grep("#Monet", tweetdatand$full_text),]
-tweets_regarding_movement_5 <- tweetdatand[grep("#LastGeneration", tweetdatand$full_text),]
-tweets_regarding_movement_6 <- tweetdatand[grep("paint", tweetdatand$full_text),]
-tweets_regarding_movement_7 <- tweetdatand[grep("Oil", tweetdatand$full_text),]
-tweets_regarding_movement_8 <- tweetdatand[grep("oil", tweetdatand$full_text),]
-
-#Eliminare una colonna specifica settandola a NULL
-#la colonna Source.Name conteneva nomi uguali per vari tweet causando errori
-tweets_regarding_movement$Source.Name <- NULL
-tweets_regarding_movement_1$Source.Name <- NULL
-tweets_regarding_movement_2$Source.Name <- NULL
-tweets_regarding_movement_3$Source.Name <- NULL
-tweets_regarding_movement_4$Source.Name <- NULL
-tweets_regarding_movement_5$Source.Name <- NULL
-tweets_regarding_movement_6$Source.Name <- NULL
-tweets_regarding_movement_7$Source.Name <- NULL
-tweets_regarding_movement_8$Source.Name <- NULL
-
-#combinare vari dataframe direttamente in r sembra non funzionare quindi convertirli prima in file excel per poi fare il merge
-write_xlsx(tweets_regarding_movement, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement.xlsx")
-write_xlsx(tweets_regarding_movement_1, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_1.xlsx")
-write_xlsx(tweets_regarding_movement_2, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_2.xlsx")
-write_xlsx(tweets_regarding_movement_3, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_3.xlsx")
-write_xlsx(tweets_regarding_movement_4, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_4.xlsx")
-write_xlsx(tweets_regarding_movement_5, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_5.xlsx")
-write_xlsx(tweets_regarding_movement_6, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_6.xlsx")
-write_xlsx(tweets_regarding_movement_7, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_7.xlsx")
-write_xlsx(tweets_regarding_movement_8, "C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovementTweets\\tweets_regarding_movement_8.xlsx")
-
 movtweets <- read_xlsx("C:\\Users\\Jimpo\\Desktop\\SocialNetworkProject\\MovTweetsSentiment.xlsx")
 mt_positive <- sum(movtweets$Sentiment=="positive")
 mt_negative <- sum(movtweets$Sentiment=="negative")
