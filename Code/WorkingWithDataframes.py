abf24111d844db76de7701a004ccf0baf6a82080
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-21 23:59:45 +0100

    Piccoli cambiamenti di commenti Rimozione Youtube Analytics API dal file APIstuff

--- Code/WorkingWithDataframes.py
+++ Code/WorkingWithDataframes.py
@@ -1,4 +1,4 @@
-#Questo file conterrà il codice in python utilizzato per creare grafi dai dataframe creati nel file in R
+#Questo file conterrà il codice in python utilizzato per creare grafi dai dataframe ricavati nel codice in R
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
