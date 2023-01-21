e4e7caf01cd35b542c4b9f604ee25efcd6431d43
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-21 23:19:33 +0100

    Rimozione cartella inutile e spostamento file

--- SocialNetworkAnalysisProject/WorkingWithDataframes.py
+++ SocialNetworkAnalysisProject/WorkingWithDataframes.py
@@ -2,4 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import cufflinks as cf
+import plotly
+import plotly.graph_objs as go
 
+
+TweetsFrequencyDF <- pd.read_excel('C:\Users\Jimpo\Desktop\SNAgithub\SocialNetworkAnalysisProject\SocialNetworkAnalysisProject\Dataframes\xlsx\TweetTotali.xsx')
