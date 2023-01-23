8c10912d6d3fbd742d7a77789f567441da7e4ca0
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-23 20:17:43 +0100

    Rimozione commento

--- Code/WorkingWithDataframes.py
+++ Code/WorkingWithDataframes.py
@@ -83,27 +83,3 @@ ttdf = pd.read_csv("C:\\Users\\Jimpo\\Desktop\\SNAgithub\\csv\\tt.csv", dtype = 
 
 fig, ax = plt.subplots()
 ttdf['created_at'].value_counts().plot(ax=ax, kind='bar')
-
-"""
-TweetsXLSX['full_text'] = TweetsXLSX['full_text'].astype(str).str.lower()
-
-regexp = RegexpTokenizer('\w+')
-
-TweetsXLSX['text_token'] = TweetsXLSX['full_text'].apply(regexp.tokenize)
-
-stopwords = nltk.corpus.stopwords.words("english")
-
-my_stopwords = ['https']
-stopwords.extend(my_stopwords)
-
-TweetsXLSX['text_token'] = TweetsXLSX['text_token'].apply(lambda x: [item for item in x if item not in stopwords])
-
-"""
-
-#commento wordcloud
-"""
-wordcloud = wordcloud(width = 600,
-                      height = 400,
-                      random_state = 2,
-                      max_font_size = 100.generate(all_words_lem))
-"""
