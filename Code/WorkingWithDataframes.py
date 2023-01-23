23317871d30dbda0fde01941328d173523334d51
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-23 10:51:18 +0100

    Modifica file .py

--- Code/WorkingWithDataframes.py
+++ Code/WorkingWithDataframes.py
@@ -6,7 +6,44 @@ import cufflinks as cf
 import plotly
 import plotly.graph_objs as go
 from IPython.display import display
+import re
+import sklearn
+from sklearn.model_selection import train_test_split
+import wordcloud
+from wordcloud import WordCloud, STOPWORDS
+import nltk
+#nltk.download()
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
-
-#TweetsXLSX = pd.read_excel("C:\\Users\\Jimpo\\Desktop\\SNAgithub\\SocialNetworkAnalysisProject\\Dataframes\\xlsx\\TweetTotali.xlsx")
+TweetsXLSX = pd.read_csv("C:\\Users\\Jimpo\\Desktop\\SNAgithub\\SocialNetworkAnalysisProject\\Dataframes\\xlsx\\TweetTotali.xlsx")
 #display(TweetsXLSX)
+
+TweetsXLSX.head()
+
+x = TweetsXLSX['full_text']
+y = TweetsXLSX['id']
+
+"""
+TweetsXLSX['full_text'] = TweetsXLSX['full_text'].astype(str).str.lower()
+
+regexp = RegexpTokenizer('\w+')
+
+TweetsXLSX['text_token'] = TweetsXLSX['full_text'].apply(regexp.tokenize)
+
+stopwords = nltk.corpus.stopwords.words("english")
+
+my_stopwords = ['https']
+stopwords.extend(my_stopwords)
+
+TweetsXLSX['text_token'] = TweetsXLSX['text_token'].apply(lambda x: [item for item in x if item not in stopwords])
+
+"""
+
+#commento wordcloud
+"""
+wordcloud = wordcloud(width = 600,
+                      height = 400,
+                      random_state = 2,
+                      max_font_size = 100.generate(all_words_lem))
+"""
