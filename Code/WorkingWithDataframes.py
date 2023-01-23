7e82160cb8d6414ac375beb43bb0fd6d4169f8a8
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-01-23 11:25:10 +0100

    Aggiunta codice per wordcloud e risultati della wordcloud PNG di positive e negative wordcloud

--- Code/WorkingWithDataframes.py
+++ Code/WorkingWithDataframes.py
@@ -10,6 +10,8 @@ import cufflinks as cf
 import plotly
 import plotly.graph_objs as go
 
+import matplotlib.pyplot as plt
+
 from IPython.display import display
 
 import re
@@ -26,13 +28,48 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.classify import SklearnClassifier
 
-ttdf = pd.read_csv("C:\\Users\\Jimpo\\Desktop\\SNAgithub\\csv\\tt.csv")
+from subprocess import check_output
+
+ttdf = pd.read_csv("C:\\Users\\Jimpo\\Desktop\\SNAgithub\\csv\\ttoa.csv")
 #display(TweetsXLSX)
 
-ttdf.head()
+data = ttdf[['tweet_text','Sentiment']]
 
-x = ttdf['full_text']
-y = ttdf['id']
+#pezzo di codice per creare un wordcloud
+
+#split del dataset in trai e test
+train,test = train_test_split(data, test_size = 0.1)
+
+#rimozione sentimenti neutrali
+train = train[train.Sentiment != "neutral"]
+
+train_pos = train[ train['Sentiment'] == 'positive']
+train_pos_text = train_pos['tweet_text']
+
+train_neg = train[ train['Sentiment'] == 'negative']
+train_neg_text = train_neg['tweet_text']
+
+def wordcloud_draw(data, color = 'black'):
+    words = ' '.join(data)
+    cleaned_word = " ".join([word for word in words.split()
+                      if 'http' not in word
+                      and not word.startswith('@')
+                      and not word.startswith('#')
+                      and word != 'RT'])
+    wordcloud = WordCloud(stopwords=STOPWORDS,
+                          background_color = color,
+                          width=2500,
+                          height=2500).generate(cleaned_word)
+    plt.figure(1, figsize=(13,13))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+    
+print("Positive words")
+wordcloud_draw(train_pos_text, 'white')
+print("Negative words")
+wordcloud_draw(train_neg_text, 'black')
+
 
 """
 TweetsXLSX['full_text'] = TweetsXLSX['full_text'].astype(str).str.lower()
